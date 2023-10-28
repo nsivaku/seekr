74b737e65f37c3e2e8d0b36cbcb2daceb392a100
Yashu Singhai <yashusi@yashus-air-2.wireless-1x.unc.edu>
2023-10-28 15:13:17 -0400

    navbar UI

--- Components/NavBar.tsx
+++ Components/NavBar.tsx
@@ -1,32 +1,38 @@
-// import React from 'react'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
-
-// const NavBar: React.FC =() => {
-//   return(
-//     <>
-//     </>
-//   )
-// }
-
-export default function Navbar() {
+import Image from 'next/image'
+export default function Home() {
   return (
-<div className="navbar bg-base-100">
-  <div className="flex-1">
+  <div>
+  <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Lost</a></li>
+        <li><a>Found</a></li>
+      </ul>
+    </div>
     <a className="btn btn-ghost normal-case text-xl">Seekr</a>
   </div>
-  <div className="flex-none gap-2">
-    <div className="form-control">
-      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-    </div>
-    <div className="dropdown dropdown-end">
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Lost</a></li>
+      <li><a>Found</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+  </div>
+  <div className="dropdown dropdown-end">
       <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
-          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          <Image src="/pfp.jpg" alt="" width="32" height="32"/>
         </div>
       </label>
-      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
         <li>
           <a className="justify-between">
             Profile
@@ -38,6 +44,6 @@ export default function Navbar() {
       </ul>
     </div>
   </div>
-</div>
+  </div>
   )
 }
