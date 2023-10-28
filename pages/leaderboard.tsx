4c0e91fb6b329325035bbe74f1d8a602b4bf4f0e
yashusi <yashusi@unc.edu>
2023-10-28 19:51:29 -0400

    leaderboard

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -2,106 +2,58 @@
 import React, {useState} from "react";
 
 export default function Leaderboard() {
-    const [lost, setLost] = useState(true);
-    const [activeTab, setActiveTab] = useState('lost')
+    // const [lost, setLost] = useState(true);
+    // const [activeTab, setActiveTab] = useState('lost')
 
-    const handleLostTabClick = () => {
-        setLost(true);
-        setActiveTab('lost')
-    }
-
+    // const handleLostTabClick = () => {
+    //     setLost(true);
+    //     setActiveTab('lost')
     return (
         <div>
             <div>
-                <h1>Leaderboard</h1>
+                <h1>Most Frequnetly Lost</h1>
             </div>
-            <div className="tabs tabs-boxed">
-                <button className="tab tab-active=false" onClick={() => handleLostTabClick()}>Lost</button> 
-                <button className="tab tab-active" onClick={() => setLost(false)}>Found</button> 
-            </div>
-            <div className="overflow-x-auto">
-                <table className="table">
-                {/* head */}
+            <div>
+                <table className="table table-zebra">
                 <thead>
                     <tr>
                     <th>Rank</th>
                     <th>Name</th>
-                    {/* <th>Job</th> */}
-                    <th># Items Returned</th>
+                    <th># Instances Lost</th>
                     </tr>
                 </thead>
-                {lost ? <tbody>
-                    {/* row 1 */}
-                    <tr className="bg-base-200">
-                    <th>1</th>
-                    <td>Adam Ganderton</td>
-                    {/* <td>Quality Control Specialist</td> */}
-                    <td>14</td>
-                    </tr>
-                    {/* row 2 */}
-                    <tr>
-                    <th>2</th>
-                    <td>Hart Hagerty</td>
-                    {/* <td>Desktop Support Technician</td> */}
-                    <td>12</td>
-                    </tr>
-                    {/* row 3 */}
-                    <tr>
-                    <th>3</th>
-                    <td>Brice Swyre</td>
-                    {/* <td>Tax Accountant</td> */}
-                    <td>11</td>
-                    </tr>
-                    <tr>
-                    <th>4</th>
-                    <td>Brice Swyre</td>
-                    {/* <td>Tax Accountant</td> */}
-                    <td>9</td>
-                    </tr>
-                    <tr>
-                    <th>5</th>
-                    <td>Brice Swyre</td>
-                    {/* <td>Tax Accountant</td> */}
-                    <td>7</td>
-                    </tr>
-                </tbody>
-                :
                 <tbody>
                     {/* row 1 */}
-                    <tr className="bg-base-200">
+                    <tr>
                     <th>1</th>
-                    <td>Cy Ganderton</td>
-                    {/* <td>Quality Control Specialist</td> */}
-                    <td>14</td>
+                    <td>AirPod(s)</td>
+                    <td>16</td>
                     </tr>
                     {/* row 2 */}
                     <tr>
                     <th>2</th>
-                    <td>Hart Hagerty</td>
-                    {/* <td>Desktop Support Technician</td> */}
+                    <td>Keys/Fobs</td>
                     <td>12</td>
                     </tr>
                     {/* row 3 */}
                     <tr>
                     <th>3</th>
-                    <td>Brice Swyre</td>
-                    {/* <td>Tax Accountant</td> */}
+                    <td>Water Bottles</td>
                     <td>11</td>
                     </tr>
                     <tr>
                     <th>4</th>
-                    <td>Brice Swyre</td>
+                    <td>Wallets/Cards</td>
                     {/* <td>Tax Accountant</td> */}
-                    <td>9</td>
+                    <td>10</td>
                     </tr>
                     <tr>
                     <th>5</th>
-                    <td>Brice Swyre</td>
+                    <td>Chargers</td>
                     {/* <td>Tax Accountant</td> */}
-                    <td>7</td>
+                    <td>8</td>
                     </tr>
                 </tbody>
-                }
                 </table>
             </div>
         </div>
