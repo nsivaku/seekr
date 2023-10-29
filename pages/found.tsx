1e533747256df0f5c564310f1c698b85ea30d3a8
nsivaku <nsivaku@unc.edu>
2023-10-28 23:24:20 -0400

    found page

--- pages/found.tsx
+++ pages/found.tsx
@@ -1,10 +1,42 @@
+import React from "react";
+import Tile from "../components/tile";
+import data from "../data2.json";
 import NavBar from "../components/NavBar";
+interface BoardProps {
+  rows: number;
+  cols: number;
+}
 
-export default function Found() {
-    return (
-        <div>
-            <NavBar />
-            <p>found page</p>
+const Board: React.FC<BoardProps> = ({ rows, cols }) => {
+  return (
+    <div>
+      <NavBar />
+      <div className="container mx-auto p-6">
+        <div className="prose my-3">
+          <h1 className="text-white">
+            Found Items
+          </h1>
         </div>
-    );
-}   
+        <div className="container mx-auto" style={{marginTop:"50px"}}>
+          <div className="board">
+            <div className="grid grid-cols-3 gap-4 ">
+              {data.images.map((tileData, j) => (
+                <Tile
+                  key={j}
+                  src={tileData.src}
+                  title={tileData.title}
+                  location={tileData.location}
+                  alt={tileData.alt}
+                  date={tileData.date}
+                  phone={tileData.phone}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Board;
