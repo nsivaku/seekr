6cf0c910c2c60c97540832405edfeab113da1ceb
nsivaku <nsivaku@unc.edu>
2023-10-28 22:51:32 -0400

    updated lost page

--- components/tile.tsx
+++ components/tile.tsx
@@ -4,23 +4,27 @@ import Image from "next/image";
 
 export default function Tile(props) {
   return (
-    <Link href="/messenger">
-      <button>
-        <div className="card card-side bg-base-100 shadow-xl">
-          <figure>
+    <div className="mx-2 h-full" >
+        <div className="card card-side bg-base-100 shadow-xl h-full">
+          <figure className="w-1/2 h-full">
             <img
               src={props.src}
               alt={props.alt}
-              className="object-contain w-full h-full"
-              style={{ maxWidth: "100%", maxHeight: "100%" }}
+              className="object-cover h-full"
             />
           </figure>
-          <div className="card-body">
-            <h2 className="card-title">{props.title}</h2>
-            <p>{props.description}</p>
+        <div className="card-body w-1/2 m-0">
+          <div className="prose">
+            <h2 className="text-white">{props.title}</h2>
+            <h4 className="text-white">Location</h4>
+            <h4>{props.location}</h4>
+            <h4 className="text-white">Found</h4>
+            <h4>{props.date}</h4>
+            <h4 className="text-white">Phone</h4>
+            <h4 className="align-bottom">{props.phone}</h4>
           </div>
         </div>
-      </button>
-    </Link>
+        </div>
+    </div>
   );
 }
